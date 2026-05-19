2bbecd40b004bfe4aa7b359f1144b2544867e6d7
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-05-19 09:57:35 +0300

    готов парсер ссылок, осталось сделать проверку получилось ли открыть ссылку

--- collection_links.py
+++ collection_links.py
@@ -6,68 +6,54 @@ from bs4 import BeautifulSoup
 domain = "https://wwwww.jodi.org/"
 
 
-class Controller:
-    def __init__(self, urls: list):
-        self.queue_links: list = urls
-        self.visited_links = set()
+class ParserLinks:
+    def __init__(self, url: str):
+        self.links:list = []
+        self.used_links = set()
+        self.url :str   = url
+        self.html:str   = self.get_html(url)
+
+    def get_html(self, url, plus_url=""):
+        if f'{url}{plus_url}' in self.used_links:
+            return "<html> </html>"
+            
+
+        if rq.get(f'{url}{plus_url}').text == 200:
+            self.used_links.add(f'{self.url}{plus_url}')
+            return rq.get(f'{url}{plus_url}').text
+        else:
+            return "<html> </html>"
+
+    def search_links(self):
+        soup = BeautifulSoup(self.html, "lxml")
+
+        tags_a = soup.find_all("a")
+        for tag_a in tags_a:
+            self.links.append(tag_a.get("href"))
 
     def run(self):
-        while len(self.queue_links) > 0:
-            current_link = self.queue_links.pop(0)
-            if current_link in self.visited_links:
-                continue
-            self.visited_links.add(current_link)
+        self.search_links()
 
-    def search_links(self, url):
-        soup = BeautifulSoup(rq.get(url).text, "lxml")
-        for tag_a in soup.find_all("a"):
-            self.queue_links.append(tag_a)
+        while len(self.links) > 0:
+            self.html = self.get_html(self.url, self.links[0])
+            self.links.pop(0)
+            self.search_links()
+            print(f'спарсено ссылок: {len(self.used_links)}')
+            print(f'спарсеные ссылки: {self.used_links}')
 
-
-class Links:
-    def __init__(self):
-        self.links = []
-        self.used_links = set()
-
-    def get(self) -> set:
-        return set(self.links)
-
-    def add(self, link):
-        self.links.append(link)
-
-
-def get_html(domain, path_url=""):
-    responce = rq.get(domain + path_url)
-    return responce.text
-
-
-def searching_links_and_add(html):
-    soup = BeautifulSoup(html, "lxml")
-    tags_a = soup.find_all("a")
-    for a in tags_a:
-        links.add(a.get("href"))
-
-
-
-def write_links_in_file(): ...
-
-
-def controller(url) -> None:
-    searching_links_and_add(get_html(url))
-    while len(links.get()) > 0:
-        break
-    for n in range(10):
-        print(links.get())
-        print(f"отрработало {n} раз")
-        for path in links.get():
-            searching_links_and_add(get_html(url, path_url=path))
-    else:
-        print(links.get())
 
 
 if __name__ == "__main__":
-    links = Links()
-    controller(domain)
+    parser = ParserLinks(domain)
+    parser.run()
+    print(parser.url)
+    print(parser.links)
+    print(parser.used_links)
+
+
+    # мой старый код
+    # links = Links()
+    # controller(domain)
 
     # код мена с чата
     # controller_class = Controller([domain])
