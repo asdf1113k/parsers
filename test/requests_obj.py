d2aaad7d4dea75291a969e49f0477e66e3bc823f
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-05-19 11:09:05 +0300

    папка для тестов library

---
+++ test/requests_obj.py
@@ -0,0 +1,6 @@
+"""узнавал назване объектов возвращаемые либой requests"""
+
+import requests as rq
+
+responce = rq.get("https://google.com")
+print(type(responce)) # <class 'requests.models.Response'>
