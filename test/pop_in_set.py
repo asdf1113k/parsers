99077573dd4c6cb736c60ee0f931ebc77c4d50cc
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-05-19 15:49:20 +0300

    удаление в колексии set

---
+++ test/pop_in_set.py
@@ -0,0 +1,13 @@
+colection = {None, 1, 1, 1, 2, 3, 3}  # в этом set нам не нужен None
+
+print(colection)
+
+colection.discard(None)
+
+print(colection)
+
+# как обращася к элементам set?
+
+list(colection)[0]
+
+# нечего лучше не придумал
