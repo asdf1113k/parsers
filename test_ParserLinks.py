a82e52ab74856d7d536c28fbe193624da747df9e
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-05-22 09:28:14 +0300

    update tests done test_set_auto_run, test_get_html, test_set_url

--- test_ParserLinks.py
+++ test_ParserLinks.py
@@ -3,11 +3,50 @@ import pytest
 from ParserLinks import Parser
 
 
-def test_ParserLinks():
+@pytest.fixture
+def test_Parser() -> Parser:
+    url = "https://google.com/"
+    return Parser(url)
+
+def test_set_url() -> None:
+    
+    with pytest.raises(ValueError):
+        url = "google.com"
+        parser = Parser(url)  # noqa: F841
+    # проверка на назначение url
     url = "https://google.com/"
     parser = Parser(url)
+    assert parser.url == "https://google.com/"
+    
+        
 
-    with pytest.raises(ValueError, match="URL должен оканчиватся на '/' "):
-        parser.get_html(url[0:-1]) # "https://google.com"
+def test_get_html(test_Parser) -> None:
+    
+    # сделать автоматическое добавлнение "/" в конце url
+    # сделать отдельный метод для этого запускающиеся в __init__
 
-    assert type(parser.links) is set  # assert переводится как утверждать
+    assert type(test_Parser.links) is list  # assert переводится как утверждать
+    test_Parser.get_html(test_Parser.url)
+    assert test_Parser.count_get_html == 1
+    assert len(test_Parser.cheaking_links) == 1
+    assert test_Parser.cheaking_links == {"https://www.google.com/"}
+
+    
+
+
+def test_set_auto_run(test_Parser) -> None:
+    with pytest.raises(ValueError):
+        test_Parser.set_auto_run(1)
+    with pytest.raises(ValueError):
+        test_Parser.set_auto_run("False")
+    
+    test_Parser.set_auto_run(True)
+    assert test_Parser.auto_run == True
+    test_Parser.set_auto_run(False)
+    assert test_Parser.auto_run == False
+
+
+    
+
+if __name__ == "__main__":
+    print("https://google.com/"[0:8])
