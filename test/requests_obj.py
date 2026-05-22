e7b6685921c0308d43e7d072a2494a0c4c75a5c1
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-05-22 14:22:47 +0300

    удалил директорию test которая такой не являлась

--- test/requests_obj.py
+++
@@ -1,6 +0,0 @@
-"""узнавал назване объектов возвращаемые либой requests"""
-
-import requests as rq
-
-responce = rq.get("https://google.com")
-print(type(responce))  # <class 'requests.models.Response'>
--- test/test_error.py
+++
@@ -1,3 +0,0 @@
-class Set(set):
-    def __getitem__(self, index: int) -> list:
-        return list(self)[index]        
