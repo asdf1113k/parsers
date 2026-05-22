c4673315cdd502005b6a7c388396107f6b04bb4c
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-05-22 09:44:35 +0300

    на данный момент коммита, есть баг при котором не происходит рекурсивный поиск. ИСПРАВИТЬ big update ParserLinks.py добавил проверку передаваемого url при инисилизации добавил аннотацию типов добавил возможность автоматического запуска парсера сразу при инисилизации fix bag [35] статус код был равен 200 а проверка если больше 200 была исправлена !!! в self.run() есть prints для отладки не забыть убрать

--- ParserLinks.py
+++ ParserLinks.py
@@ -1,31 +1,47 @@
 # парсер с рекурсивным парсингом ссылок и без него V2
 
-from requests import get
+from requests import Response, get
 
 from bs4 import BeautifulSoup
 
 class Parser:
-    def __init__(self, url, recursion_search: bool=False):
-        self.url = url
-        self.html = ""
+    def __init__(self, url, recursion_search: bool=False, auto_run=False) -> None:
+        self.url: str = self.set_url(url)
+        self.html: str = ""
         self.links = []
         self.cheaking_links = set()
         self.count_recursion = 300
         self.count_get_html = 0
+
+        if auto_run:
+            self.run()
     
-    def get_html(self, url):
+    def set_url(self, new_url:str) -> str:
+        if new_url.startswith("https://", 0, 8) and new_url[-1] == "/":
+            return new_url
+        else: 
+            raise ValueError(f"переданный url({new_url}) не соответствует качествам url. пример как должен выглядеть url (https://google.com/)")
+            
+    def set_auto_run(self, new_value:bool) -> None:
+        if type(new_value) is bool:
+            self.auto_run = new_value
+        else:
+            raise ValueError(f"в {self.set_auto_run.__name__} передано не bool значение")
+
+    def get_html(self, url) -> None:
         self.count_get_html += 1
     
-        response = get(url)
-        if 200< response.status_code < 399:
+        response: Response = get(url)
+        if 200 <= response.status_code <= 399:
             if url not in self.cheaking_links:
-                self.html = response.text
+                self.html: str = response.text
                 self.cheaking_links.add(response.url)
-            else:
-                self.html = "<html></html>"
                 return
+        else:
+            self.html = "<html></html>"
+            return
 
-    def search_links(self):
+    def search_links(self) -> None:
         soup = BeautifulSoup(self.html, "lxml")
         for tag in soup.find_all():
             if tag.get("href") is None:
@@ -35,17 +51,29 @@ class Parser:
 
                 
     
-    def run(self):
+    def run(self) -> None:
         self.get_html(self.url)
         self.search_links()
-        # while len(self.links) > 0:
-            # recursion_links = self.links
-            # for link in recursion_links:
+        while len(self.links) > 0:
+            print(f"запросов сделано            : {self.count_get_html}")
+            print(f"ссылок на очередь парсинга  : {len(self.links)}")
+            print(f"спарсеные ссылки            : {len(self.cheaking_links)}")
+            print(f"links :{self.links}")
+ 
+            recursion_links = self.links
+            for link in recursion_links:
+                self.get_html(link)
+                self.search_links
+
                 
 
             
 if __name__ == "__main__":
-    ...
+    url = "https://wwwww.jodi.org/"
+    parser = Parser(url, auto_run=True)
+    print(f"запросов сделано            : {parser.count_get_html}")
+    print(f"ссылок на очередь парсинга  : {len(parser.links)}")
+    print(f"спарсеные ссылки            : {len(parser.cheaking_links)}")
  
 
 
