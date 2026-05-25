332d36fc6014650e5dc592ad1299915633e9e8d4
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-05-25 09:49:51 +0300

    добавил тесты, но они до конца не до деланы

--- test_ParserLinks.py
+++ test_ParserLinks.py
@@ -1,52 +1,55 @@
 import pytest
 
-from ParserLinks import Parser
+from parsers import ParserLinks
 
 
 @pytest.fixture
-def test_Parser() -> Parser:
+def class_ParserLinks() -> ParserLinks:
     url = "https://google.com/"
-    return Parser(url)
+    return ParserLinks(url)
 
-def test_set_url() -> None:
-    
-    with pytest.raises(ValueError):
-        url = "google.com"
-        parser = Parser(url)  # noqa: F841
-    # проверка на назначение url
-    url = "https://google.com/"
-    parser = Parser(url)
-    assert parser.url == "https://google.com/"
-    
-        
 
-def test_get_html(test_Parser) -> None:
-    
+
+
+
+def test_get_html(class_ParserLinks) -> None:
+
     # сделать автоматическое добавлнение "/" в конце url
     # сделать отдельный метод для этого запускающиеся в __init__
 
-    assert type(test_Parser.links) is list  # assert переводится как утверждать
-    test_Parser.get_html(test_Parser.url)
-    assert test_Parser.count_get_html == 1
-    assert len(test_Parser.cheaking_links) == 1
-    assert test_Parser.cheaking_links == {"https://www.google.com/"}
+    assert (
+        type(class_ParserLinks.link_queue) is list
+    )  # assert переводится как утверждать
+    class_ParserLinks.get_html(class_ParserLinks.url)
+    assert class_ParserLinks.count_get_html == 1
+    assert len(class_ParserLinks.cheaking_links) == 1
+    assert class_ParserLinks.cheaking_links == {"https://www.google.com/"}
 
-    
 
-
-def test_set_auto_run(test_Parser) -> None:
+def test_set_auto_run(class_ParserLinks) -> None:
     with pytest.raises(ValueError):
-        test_Parser.set_auto_run(1)
+        class_ParserLinks.set_auto_run(1)
     with pytest.raises(ValueError):
-        test_Parser.set_auto_run("False")
-    
-    test_Parser.set_auto_run(True)
-    assert test_Parser.auto_run == True
-    test_Parser.set_auto_run(False)
-    assert test_Parser.auto_run == False
+        class_ParserLinks.set_auto_run("False")
+
+    assert class_ParserLinks.set_auto_run(True) == True  # noqa: E712
+    assert class_ParserLinks.auto_run == True  # noqa: E712
+    assert class_ParserLinks.set_auto_run(False) == False  # noqa: E712
+    assert class_ParserLinks.auto_run == False  # noqa: E712
 
 
-    
+def test_parsing_page():
+    parser = ParserLinks("http://192.168.1.16:5500/www/", auto_run=True)
+
 
 if __name__ == "__main__":
-    print("https://google.com/"[0:8])
+    # print("https://google.com/"[0:8])
+    parser = ParserLinks(
+        "http://192.168.1.16:5500/www/",
+        auto_run=True,
+    )
+    # print(f"запросов сделано            : {parser.count_get_html}")
+    # print(f"ссылок на очередь парсинга  : {len(parser.link_queue)}")
+    # print(parser.link_queue)
+    # print(f"спарсеные ссылки            : {len(parser.cheaking_links)}")
+    # print(parser.cheaking_links)
